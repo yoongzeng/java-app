0e67e8c4905a2e99ca89d1eb54922c213dd60eeb
Yoong Zeng <chong.yoongzeng@gmail.com>
2021-01-02 16:29:43 +0800

    First Commit

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+package com.sanumala;
+public class helloworld
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello gist @ GitHub!");   
+      }
+} 
+
